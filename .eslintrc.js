0dc50ec2df0484bf3d0937cf9bb7e02fd26e7047
Szilard Tumo <szilard.tumo@3ss.tv>
2023-10-05 16:03:50 +0300

    Configured ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: 'module',
+  },
+  settings: {
+    react: { version: 'detect' },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  ignorePatterns: ['node_modules/*'],
+  extends: [
+    'airbnb',
+    'next/core-web-vitals',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:import/recommended',
+    'plugin:testing-library/react',
+    'plugin:jest-dom/recommended',
+  ],
+  rules: {
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'import/no-duplicates': 'error',
+    'import/order': [
+      'error',
+      {
+        pathGroups: [
+          {
+            pattern: '@/**',
+            group: 'external',
+            position: 'after',
+          },
+        ],
+        groups: [
+          ['external', 'builtin'],
+          ['internal', 'index', 'sibling', 'parent', 'object'],
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+      extends: ['airbnb-typescript', 'prettier', 'plugin:import/typescript'],
+    },
+  ],
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-import type { Config } from 'tailwindcss'
+import typographyPlugin from '@tailwindcss/typography';
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -10,11 +11,10 @@ const config: Config = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [typographyPlugin],
+};
+export default config;
